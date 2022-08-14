f7d1f479eda1950e9a8d6772ac465837e298f39e
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2022-08-14 17:09:41 -0400

    Combined some components into one

--- src/Current.js
+++ src/Current.js
@@ -1,7 +1,4 @@
 import React from "react";
-import CurrentTemp from "./CurrentTemp";
-import CurrentWeatherDeet from "./CurrentWeatherDeet";
-import CurrentLocation from "./CurrentLocation";
 import "./Current.css";
 
 export default function Current() {
@@ -17,15 +14,36 @@ export default function Current() {
         </div>
         <div className="col-2">
           {/* Current temperature */}
-          <CurrentTemp value={19} />
+          <div className="clearfix">
+            <span className="current-temperature float-left">20</span>
+            <small className="unit">
+              <span className="active float-left">°C</span>
+            </small>
+          </div>
         </div>
         <div className="col-3 weather-detail">
           {/* Current Weather Detail */}
-          <CurrentWeatherDeet wind={10} cloudiness={20} humidity={50} />
+          <ul>
+            <li className="wind">
+              Wind: <span>10</span> m/h
+            </li>
+            <li className="cloudiness">
+              Cloudiness: <span>30</span> %
+            </li>
+            <li className="humidity">
+              Humidity:<span>24</span> %
+            </li>
+          </ul>
         </div>
         <div className="col current-location-info">
           {/* Current location */}
-          <CurrentLocation location="Tokyo" date="Tue, Aug 9th" />
+          <ul>
+            <li className="current-location">Courtenay</li>
+            <li>
+              last updated: <span>August 14th</span>
+            </li>
+            <li></li>
+          </ul>
         </div>
       </div>
     </div>

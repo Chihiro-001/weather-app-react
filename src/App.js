3ad1c5e422f6080ee19b67fa3e4e38d4cfc898f1
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2022-08-15 19:13:47 -0400

    Imported react bootstrap and added App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Input from "./Input";
 import Current from "./Current";
+import "./App.css";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
         <div class="weather-app today">
           <Input />
           <Current />

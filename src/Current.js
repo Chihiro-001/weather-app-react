f627cb05e8b8722f6947001fb5b539193b4b13ef
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2022-08-16 17:50:59 -0400

    Updated components with api responses

--- src/Current.js
+++ src/Current.js
@@ -1,60 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Current.css";
 
 export default function Current() {
+  const [temperature, setTemperature] = useState(null);
+  const [weatherData, setWeatherDate] = useState({ loaded: false });
   function handleResponse(response) {
-    console.log(response.data);
+    // console.log(response.data);
+    setTemperature(Math.round(response.data.main.temp));
+    setWeatherDate({
+      loaded: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      cloudiness: response.data.clouds.all,
+      humidity: response.data.main.humidity,
+      updatedDate: response.data.lastupdate.value,
+    });
   }
-  const city = "Tokyo";
-  const apiKey = "7b2471b32a9aba35093d93a82db55ee8";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&
-  units= metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="Current">
-      <div className="row align-items-start">
-        <div class="col-2">
-          <img
-            src="https://openweathermap.org/img/wn/10d@2x.png"
-            class="current-weather-icon"
-            alt="current weather icon"
-          />
-        </div>
-        <div className="col-2">
-          {/* Current temperature */}
-          <div className="clearfix">
-            <span className="current-temperature float-left">20</span>
-            <small className="unit">
-              <span className="active float-left">°C</span>
-            </small>
+  if (weatherData.loaded) {
+    return (
+      <div className="Current">
+        <div className="row align-items-start">
+          <div class="col-2">
+            <img
+              src="https://openweathermap.org/img/wn/10d@2x.png"
+              class="current-weather-icon"
+              alt="current weather icon"
+            />
+          </div>
+          <div className="col-2">
+            {/* Current temperature */}
+            <div className="clearfix">
+              <span className="current-temperature float-left">
+                {temperature}
+              </span>
+              <small className="unit">
+                <span className="active float-left">°C</span>
+              </small>
+            </div>
+          </div>
+          <div className="col-3 weather-detail">
+            {/* Current Weather Detail */}
+            <ul>
+              <li className="wind">
+                Wind: <span>{weatherData.wind}</span> m/h
+              </li>
+              <li className="cloudiness">
+                Cloudiness: <span>{weatherData.cloudiness}</span> %
+              </li>
+              <li className="humidity">
+                Humidity:<span>{weatherData.humidity}</span> %
+              </li>
+            </ul>
+          </div>
+          <div className="col current-location-info">
+            {/* Current location */}
+            <ul>
+              <li className="current-location">Tokyo</li>
+              <li>
+                Time: <span>{weatherData.lastupdate}</span>
+              </li>
+              <li></li>
+            </ul>
           </div>
         </div>
-        <div className="col-3 weather-detail">
-          {/* Current Weather Detail */}
-          <ul>
-            <li className="wind">
-              Wind: <span>10</span> m/h
-            </li>
-            <li className="cloudiness">
-              Cloudiness: <span>30</span> %
-            </li>
-            <li className="humidity">
-              Humidity:<span>24</span> %
-            </li>
-          </ul>
-        </div>
-        <div className="col current-location-info">
-          {/* Current location */}
-          <ul>
-            <li className="current-location">Courtenay</li>
-            <li>
-              last updated: <span>August 14th</span>
-            </li>
-            <li></li>
-          </ul>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const city = "Tokyo";
+    const apiKey = "7b2471b32a9aba35093d93a82db55ee8";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return <div className="Current">Loading...</div>;
+  }
 }
